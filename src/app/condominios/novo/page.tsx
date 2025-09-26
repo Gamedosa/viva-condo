14ee672b3f2f158418e9edb6bbc24784c7dae1a9
GUILHERME RIOS SANTOS <1050482413037@fatec>
2025-09-26 20:46:14 -0300

    msg

--- src/app/condominios/novo/page.tsx
+++ src/app/condominios/novo/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-export default function ListaCondompinios() {
-    return(
-        <div>
-            <h1>
-            </h1>
-        </div>
-    );
+export default function Novo() {
+
+  return (
+    <div id="principal" className="min-h-screen flex items-center justify-center bg-black">
+        <h1 id="name" className="text-2xl font-bold text-center">
+          novo condominios
+        </h1>
+    </div>
+  );
 }
--- src/services/api-condominios.ts
+++ src/services/api-condominios.ts
@@ -1,14 +1,3 @@
-export interface ICondominio {
-    id_condominio: string;
-    nome_condominio: string;
-    endereco_condominio: string;
-    cidade_condominio: string;
-    uf_condominio: string;
-    tipo_condominio: string;
-    created_at: string;
-    id_cliente: number;
-}
-
 export const getCondominios = async () => {
     const response = await fetch('https://raw.githubusercontent.com/vagner107/viva-condo/refs/heads/main/src/app/condominios/api_condominio.json')
 
